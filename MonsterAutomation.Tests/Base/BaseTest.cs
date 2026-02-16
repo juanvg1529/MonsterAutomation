5d2a97146a73d2b1d4ad7ab34bf5078fed8b6288
JuanDVG <rockjdvg96@hotmail.com>
2026-02-16 11:00:05 -0500

    basestest created

--- MonsterAutomation.Tests/Base/BaseTest.cs
+++ MonsterAutomation.Tests/Base/BaseTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,50 @@ using System.Threading.Tasks;
 
 namespace MonsterAutomation.Tests.Base
 {
-    internal class BaseTest
+    public abstract class BaseTest : PageTest
     {
+
+        protected IPlaywright playwright;
+        protected IBrowser browser;
+        protected IBrowserContext context;
+        protected IPage page;
+
+        protected const string BaseUrl = "http://localhost:3000/";
+
+
+
+        [OneTimeSetUp]
+        public async Task GlobalSetup()
+        {
+            playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions()
+            {
+                Headless = false
+            });
+        }
+
+
+        [SetUp]
+        public async Task Setup()
+        {
+            context = await browser.NewContextAsync();
+            page = await browser.NewPageAsync();
+
+            await page.GotoAsync(BaseUrl);
+        }
+
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await context.CloseAsync();
+        }
+
+        [OneTimeTearDown]
+        public async Task GlobalTearDown()
+        {
+            await browser.CloseAsync();
+            playwright.Dispose();
+        }
     }
 }
