0f84323a5d531a54d17aea4e215413383eb5582f
JuanDVG <rockjdvg96@hotmail.com>
2026-02-16 10:53:18 -0500

    initial project created and model

---
+++ MonsterAutomation.Tests/Models/MonsterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterAutomation.Tests.Models
+{
+    public sealed class MonsterModel
+    {
+        public string Name {  get; set; }
+        public int Hp {  get; set; }
+        public int Attack {  get; set; }
+        public int Defense {  get; set; }
+        public int Speed {  get; set; }
+    }
+}
---
+++ MonsterAutomation.Tests/Tests/CreationMonstersTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterAutomation.Tests.Tests
+{
+    internal class CreationMonstersTest
+    {
+    }
+}
